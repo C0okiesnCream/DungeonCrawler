f44a8b624287c20b2c20f1976ed3380250bb5c31
C0okiesnCream <69062323+C0okiesnCream@users.noreply.github.com>
2021-12-21 12:50:13 -0700

    making testmob and folders for NPCs

--- Dungeon Crawler.py
+++ Dungeon Crawler.py
@@ -1,9 +1,42 @@
 #imports
-import random
 import colorama
-import py_cui
+import json
+import random
+from time import sleep
+from pynput import keyboard
 
-#py_cui menu screen
-class menu:
-    def __init__(self, master : py_cui.PyCUI(8, 8)):
-        self.master = master
+#classes
+class testMob():
+    def __init__(self, health, head, chest, hands, legs, feet, damage, weapon, target, roomXY, dungeonXY, dead):
+        self.health = health
+        self.head = head
+        self.chest = chest
+        self.hands = hands
+        self.legs = legs
+        self.feet = feet
+        self.damage = damage
+        self.weapon = weapon
+        self.target = target
+        self.roomXY = roomXY
+        self.dungeonXY = dungeonXY
+        self.dead = dead
+
+    def attack(self):
+        #variables
+        s = self
+        damage = 0
+        
+        #making sure that the monster has a weapon
+        if s.weapon != None:
+            damage = s.damage = s.weapon.damage
+        else:
+            damage = s.damage
+        
+        #should return the damage from the if-else above
+        return damage
+    
+    def deathCheck(self):
+        s = self
+        if s.health < 1:
+            s.dead = 1
+            
